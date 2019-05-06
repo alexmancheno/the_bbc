f4c3169b1c52c4fd664ee4719d2a0961bd215c3a
alexmancheno <alex.mancheno@gmail.com>
2019-05-06 17:44:24 -0400

    as

--- application/index.py
+++ application/index.py
@@ -6,8 +6,11 @@ app = Flask(__name__)
 # Goal is to return JSON after using the methods in datascience.py
 @app.route('/regression')
 def hello_world():
-    query = 'Select mr.Date, Mortgage_Rate, Close from BBC.Mortgage_Rates as mr inner join BBC.Mortgage_Backed_Securities as mbs on mr.date = mbs.date;'
+    query = 'Select mr.Date, mr.Mortgage_Rate, mbs.Close from BBC.Mortgage_Rates as mr inner join BBC.Mortgage_Backed_Securities as mbs on mr.date = mbs.date;'
 
     results = linear_regression(query)
     
     return jsonify(results)
+
+if __name__ == '__main__':
+    app.run(port=8080)

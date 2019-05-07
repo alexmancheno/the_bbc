9240be6a760aa131f3bfdf176f845019e15149ec
alexmancheno <alex.mancheno@gmail.com>
2019-05-06 20:12:50 -0400

    asd

--- application/datascience.py
+++ application/datascience.py
@@ -34,7 +34,7 @@ def linear_regression(s):
 
     # query database and return results as a DataFrame
     x = query(s)
-
+    
     # clean the format of the 'Date' column
     x['Date'] = pd.to_datetime(x['Date'])
 
@@ -49,7 +49,7 @@ def linear_regression(s):
 
     # run a linear regression holding out 20% of data as test data
     lm = linear_model.LinearRegression()
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0.2)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
     lm.fit(x_train, y_train)
     y_pred = lm.predict(x_test)
     results['holdout_linear_regression_score'] = lm.score(x, y)
@@ -58,7 +58,6 @@ def linear_regression(s):
     kf = KFold(n_splits=5, random_state=None, shuffle=False)
     kf.get_n_splits(x)
     scores = []
-    best_svr = SVR(kernel='rbf')
     for train_index, test_index in kf.split(x):
         x_train, x_test = x.iloc[train_index], x.iloc[test_index]
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
@@ -66,6 +65,31 @@ def linear_regression(s):
         model = lm.fit(x_train, y_train)
         predictions = lm.predict(x_test)
         scores.append(lm.score(x, y))
-    plt.savefig('test.png')
     results['kfolds_linear_regression_score'] = np.mean(scores)
+
+    # start predicting!
+    # run linear regressions for each independent variable
+    
+    independent_var_predictions = {}
+    local_x = x.filter(['Date'])
+    for column in x.drop('Date', axis=1):
+        local_y = x.filter([column])
+        lm = linear_model.LinearRegression()
+        lm.fit(local_x, local_y)
+        offset = x['Date'].values[-1] - x['Date'].values[-2]
+        length = x.index.max()
+        arr = []
+        print(lm.coef_)
+        print(lm.intercept_)
+        # for i in range(1, 6):
+
+
+    # append the next 5 rows to the x-axis and y-axis
+    offset = x['Date'].values[-1] - x['Date'].values[-2]
+    length = x.index.max()
+    # print('offset: ', offset, ' length: ', length)
+
+    # for i in range(1, 6):
+    #     x[length + i] = 
+
     return results
--- application/index.py
+++ application/index.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 # Goal is to return JSON after using the methods in datascience.py
 @app.route('/regression')
 def hello_world():
+
     query = 'Select mr.Date, mr.Mortgage_Rate, mbs.Close from BBC.Mortgage_Rates as mr inner join BBC.Mortgage_Backed_Securities as mbs on mr.date = mbs.date;'
 
     results = linear_regression(query)
@@ -13,4 +14,4 @@ def hello_world():
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(port=8080)
+    app.run(host='localhost', port=8080)
